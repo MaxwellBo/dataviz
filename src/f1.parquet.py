a6023db996053f4b0528bcf9ab7e2405d31b23ce
Max Bo <mjbo@canva.com>
2025-03-18 17:38:47 +1100

    PQ

---
+++ src/f1.parquet.py
@@ -0,0 +1,33 @@
+
+"""
+Position changes during a race
+==============================
+
+Plot the position of each driver at the end of each lap.
+"""
+
+import matplotlib.pyplot as plt
+
+import fastf1.plotting
+import pandas as pd
+
+# Load FastF1's dark color scheme
+fastf1.plotting.setup_mpl(mpl_timedelta_support=False, misc_mpl_mods=False,
+                          color_scheme='fastf1')
+
+
+##############################################################################
+# Load the session and create the plot
+session = fastf1.get_session(2023, 2, 'R')
+session.load(telemetry=True, weather=True)
+
+df = pd.DataFrame()
+
+for driver in session.drivers:
+  driver_pos = session.laps.pick_drivers(driver).get_pos_data().add_driver_ahead()
+  driver_pos['DriverId'] = driver
+  driver_pos['DriverName'] = session.get_driver(driver).FullName
+  df = pd.concat([df, driver_pos], ignore_index=True)
+
+print(df.to_parquet())
+
