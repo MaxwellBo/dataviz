8f9bd9684bf1a84160354ff3750d1feaef202f56
Max Bo <mjbo@canva.com>
2025-03-18 17:18:57 +1100

    More

--- src/f1.json.py
+++ src/f1.json.py
@@ -9,6 +9,7 @@ Plot the position of each driver at the end of each lap.
 import matplotlib.pyplot as plt
 
 import fastf1.plotting
+import pandas as pd
 
 # Load FastF1's dark color scheme
 fastf1.plotting.setup_mpl(mpl_timedelta_support=False, misc_mpl_mods=False,
@@ -17,12 +18,16 @@ fastf1.plotting.setup_mpl(mpl_timedelta_support=False, misc_mpl_mods=False,
 
 ##############################################################################
 # Load the session and create the plot
-session = fastf1.get_session(2023, 1, 'R')
+session = fastf1.get_session(2023, 2, 'R')
 session.load(telemetry=True, weather=True)
 
-print(session.laps.pick_drivers('1').get_pos_data().add_driver_ahead().to_json())
+df = pd.DataFrame()
 
+for driver in session.drivers:
+  driver_pos = session.laps.pick_drivers(driver).get_pos_data().add_driver_ahead()
+  driver_pos['DriverId'] = driver
+  driver_pos['DriverName'] = session.get_driver(driver).FullName
+  df = pd.concat([df, driver_pos], ignore_index=True)
 
-# create a "tidy" version of the data - we want every single driver in the session
-# and we want each row to also have a driver column
+print(df.to_json())
 
