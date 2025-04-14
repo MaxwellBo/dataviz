6a37e15cac71cc384220bf9d974d3c61c43a7ee4
Max Bo <mjbo@canva.com>
2025-04-14 12:05:45 +1000

    Intermediate

--- src/f1.json.sh
+++
@@ -1,8 +0,0 @@
-jupyter nbconvert --to python src/notebook.ipynb --stdout | python3
-
-# invalidate
-#invalidate
-# invalidate
-# invalidate
-# invalidate
-# invalidate
--- src/f1.parquet.sh
+++ src/f1.parquet.sh
@@ -6,3 +6,5 @@ jupyter nbconvert --to python src/notebook.ipynb --stdout | python3
 #invalidate
 #invalidate
 #invalidate
+#invalidate
+#invalidate
